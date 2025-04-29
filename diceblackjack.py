bfea2a9a47a4157b0922ae1b2ed34730734c0072
blueSparrow2000 <86525009+blueSparrow2000@users.noreply.github.com>
2025-04-29 22:07:58 +0900

    Initial commit

---
+++ diceblackjack.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pygame
+import random
+
+from pygame.transform import threshold
+
+
+class DBJ():
+    burst_num = 21
+    def __init__(self):
+        self.player_hand=[]
+        self.dealer_hand=[]
+        self.dice=[i for i in range(6)]
+
+    def compare(self,a,b):
+        return float(a>b)-float(a<b)
+
+    def roll(self):
+        return [int(random.choice(self.dice)) for i in range(2)]
+
+    def sum_hand(self,h):
+        return sum(h)
+
+    def is_burst(self,h):
+        return self.sum_hand(h) > DBJ.burst_num
+
+    def hand_score(self,h):
+        return 0 if self.is_burst(h) else self.sum_hand(h)
+
+    def get_observation(self):
+        return self.hand_score(self.player_hand)
+
+    def reset(self):
+        self.player_hand=[]
+        self.dealer_hand=[]
+
+    '''
+    action is expected to be 1 (hit) or 0 (stand)
+    '''
+    def step(self,action):
+        reward=0
+        done=False
+        if action: # hit
+            self.player_hand += self.roll()
+            if self.is_burst(self.player_hand):
+                done=True
+                reward=-1
+        else: # stand
+            done=True
+            stop_dealer_hit_threshold = self.sum_hand(self.player_hand)
+            while self.sum_hand(self.dealer_hand) < stop_dealer_hit_threshold: # hit
+                self.dealer_hand += self.roll()
+            reward = self.compare(self.hand_score(self.player_hand), self.hand_score(self.dealer_hand))
+
+        return self.get_observation(), reward, done
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
